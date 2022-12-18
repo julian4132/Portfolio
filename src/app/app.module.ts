02688caecd01cd20b6c4bc71d8c241c5f5cc368b
Julian <juliaximo@gmail.com>
2022-12-17 22:19:44 -0300

    Main menu divided in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { CircleProgressComponent } from './circle-progress/circle-progress.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { SoftskillsComponent } from './components/softskills/softskills.component';
 
 
 
@@ -14,7 +18,11 @@ import { HeaderComponent } from './components/header/header.component';
   declarations: [
     AppComponent,
     CircleProgressComponent,
-    HeaderComponent
+    HeaderComponent,
+    ExperienceComponent,
+    EducationComponent,
+    LanguagesComponent,
+    SoftskillsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/softskills/softskills.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-softskills',
+  templateUrl: './softskills.component.html',
+  styleUrls: ['./softskills.component.scss']
+})
+export class SoftskillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  progressbarValue = 0;
+  curSec: number = 0;
+  title = 'Portfolio';
+
+
+
+  startTimer(seconds:number){
+    const timer = interval(1000);
+    const sub = timer.subscribe((sec)=>{
+      this.progressbarValue = sec * 100 / seconds;
+      this.curSec = sec;
+
+      if (this.curSec === seconds) {
+        sub.unsubscribe();
+      }
+  })
+  }
+
+}
