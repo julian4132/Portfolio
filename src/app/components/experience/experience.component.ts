21639e9535362a26400e0dc2be3f09cf6e982567
Julian <juliaximo@gmail.com>
2023-01-12 03:53:37 -0300

    Now information is retrieved from the database if the user is not logged in

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
 import { PortfolioItemInfo } from '../portfolio-item/portfolio-item.component';
 
@@ -9,13 +9,7 @@ import { PortfolioItemInfo } from '../portfolio-item/portfolio-item.component';
 })
 export class ExperienceComponent implements OnInit {
 
-  experienceProps: PortfolioItemInfo[]=
-                   [{"title":"Don Marca",
-                    "subtitle":"Desarrollador backend",
-                    "description":"Llevado adelante en el marco del programa de prácticas profesionales del Instituto Politécnico. Integración a un equipo de trabajo e interacción directa con el cliente.",
-                    "extraInfo": "Junio 2022 - Noviembre 2022\n\nPráctica Profesional",
-                    "imgSrc": "assets/images/logodonmarca.png"}
-                  ];
+  @Input() experienceProps: PortfolioItemInfo[]=[]
 
   constructor() { }
 
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchPortfolioDataService } from 'src/app/services/fetch-portfolio-data.service';
 import { headerData } from '../header/header.component';
+import { PortfolioItemInfo } from '../portfolio-item/portfolio-item.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -9,11 +10,24 @@ import { headerData } from '../header/header.component';
 })
 export class PortfolioComponent implements OnInit {
 
-  portfolioData:{headerData:headerData}={
+  portfolioData:{headerData:headerData,
+                experienceData:{
+                  experienceData:[PortfolioItemInfo]
+                }}={
                 "headerData": {
                 "fullname":"Julián Máximo Cabrera",
                 "profession":"Full Stack Developer",
-                "aboutText":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}};
+                "aboutText":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+                
+                "experienceData": {
+                  "experienceData":
+                    [{"title":"Don Marca",
+                    "subtitle":"Desarrollador backend",
+                    "description":"Llevado adelante en el marco del programa de prácticas profesionales del Instituto Politécnico. Integración a un equipo de trabajo e interacción directa con el cliente.",
+                    "extraInfo": "Junio 2022 - Noviembre 2022\n\nPráctica Profesional",
+                    "imgSrc": "assets/images/logodonmarca.png"}
+                ]
+              }};
     
   constructor(private dataService:FetchPortfolioDataService) {}
 
