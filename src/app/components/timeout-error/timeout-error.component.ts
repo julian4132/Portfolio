c86ea5b749fbdd22657027e9462d3973f9960ddb
Julian <juliaximo@gmail.com>
2023-01-31 20:16:16 -0300

    Added reload button for the user to try again.

--- src/app/components/timeout-error/timeout-error.component.ts
+++ src/app/components/timeout-error/timeout-error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-timeout-error',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeoutErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToPage(dir:string){
+    this.route.navigate([dir]);
   }
 
 }
